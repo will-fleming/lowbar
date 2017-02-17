c612f35ce6aad508c5c776e6defcdf1ba6932f5c
William Fleming <will.fleming12@btinternet.com>
2017-02-17 11:09:14 +0000

    [indexOf] update to return index of value

--- spec/advanced-lowbar.spec.js
+++ spec/advanced-lowbar.spec.js
@@ -11,6 +11,13 @@ describe('_', function () {
         });
         it('returns -1 if value is not in array', function () {
             expect(_.indexOf([1,2,3], 4)).to.equal(-1);
+            expect(_.indexOf([], 0), 0).to.equal(-1);
+        });
+        it('returns the index of value', function () {
+            expect(_.indexOf([1,2,3], 2)).to.equal(1);
+            expect(_.indexOf([5,3,78,4,3,6,1,4], 78)).to.equal(2);
+            expect(_.indexOf([1,2,3,8,6,5], 5)).to.equal(5);
+            expect(_.indexOf([1], 1)).to.equal(0);
         });
     });
 
