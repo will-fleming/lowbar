199f72a677b34444b8632f3cc32bf6c512aab13a
William Fleming <will.fleming12@btinternet.com>
2017-03-22 23:13:03 +0000

    include all of the previous lowbar functions

--- advanced-lowbar.js
+++ advanced-lowbar.js
@@ -1,93 +1,222 @@
-var _ = {};
-
-function simpleSearch (list, item) {
-    for (var i = 0; i < list.length; i++) {
-        if (list[i] === item) {
-            return i;
-        }
-    }
-    
-    return -1;
-}
-
-function binarySearch (list, item) {
+function binarySearch (list, value) {
     var s = 0;
     var e = list.length - 1;
+
     for (var loop = 0; loop < 8; loop++) {
         var m = Math.floor((s + e) / 2);
-        if (list[m] === item) {
+    
+        if (list[m] === value) {
             return m;
         }
-        if (list[m] > item) {
+
+        if (list[m] > value) {
             e = m - 1;
-        }
-        if (list[m] < item) {
+        } 
+
+        if (list[m] < value) {
             s = m + 1;
         }
     }
+
     return -1;
 }
 
-_.indexOf = function (array, value, isSorted = false) {
-    if (isSorted) {
-        return binarySearch(array, value);
-    } else {
-        return simpleSearch(array, value);
+const _ = {};
+
+_.identity = function (value) {
+  return value;
+}
+
+_.first = function (array, n = 1) {
+  if (n === 1) return array[0];
+
+  const res = [];
+  for (let i = 0; i < n && i < array.length; i++) {
+    res.push(array[i]);
+  }
+  return res;
+}
+
+_.last = function (array, n = 1) {
+  if (n === 1) return array[array.length - 1];
+
+  const res = [];
+  for (let i = 0; i < n && i < array.length; i++) {
+    res.unshift(array[array.length - 1 - i]);
+  }
+  return res;
+}
+
+_.each = function (list, iteratee, context) {
+  if (typeof iteratee !== 'function') return list;
+  iteratee = iteratee.bind(context);
+
+  if (Array.isArray(list) || typeof list === 'string') {
+    for (let i = 0; i < list.length; i++) {
+      iteratee(list[i], i, list);
     }
-};
-
-_.once = function (func) {
-    var allowed = true;    
-
-    return function () {
-        if (allowed) {
-            allowed = false;
-            return func.apply(null, arguments);
-        }
-    };
-};
-
-_.memoize = function (func) {
-    var cache = {};
-
-    return function () {
-        if (cache[arguments[0]]) {
-            return cache[arguments[0]];
-        }
-
-        var result = func.apply(null, arguments);
-        cache[arguments[0]] = result;
-        return result;   
-    };
-};
-
-_.delay = function (func, wait) {
-    var args = [];
-
-    for (var i = 2; i < arguments.length; i++) {
-        args.push(arguments[i]);
+  } else {
+    for (key in list) {
+      iteratee(list[key], key, list);
     }
+  }
+  return list;
+}
 
-    setTimeout(function () {
-        return func.apply(null, args);
-    }, wait);
-};
+_.indexOf = function (array, value, isSorted) {
+  if (isSorted) {
+    return binarySearch(array, value);
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === value) return i;
+    }
+  }
 
-_.shuffle = function (list) {
-    var result = [];
-   var copy = list.slice();
-    while (copy.length !== 0) {
-       var random = Math.floor(Math.random() * copy.length);
-       result.push(copy[random]);
-       copy.splice(random, 1);
-   }
-   return result;
-};
+  return -1;
+}
 
-_.invoke = function () {
+_.filter = function (list, predicate, context) {
+  if (typeof predicate !== 'function') predicate = function () { return true };
+  predicate = predicate.bind(context);
+
+  const res = [];
+  _.each(list, function (element, index, list) {
+    if (predicate(element, index, list)) res.push(element);
+  });
+  return res;
+}
+
+_.reject = function (list, predicate, context) {
+  if (typeof predicate !== 'function') predicate = function () { return true };
+  predicate = predicate.bind(context);
+
+  const res = [];
+  _.each(list, function (element, index, list) {
+    if (!predicate(element, index, list)) res.push(element);
+  });
+  return res;
+}
+
+_.uniq = function (array, isSorted, iteratee) {
+  let res = [];
+  let seen = [];
+
+  _.each(array, function (value, i, array) {
+    let computed = iteratee ? iteratee(value, i, array) : value;
     
-};
+    if (isSorted) {
+      if (!i || seen !== computed) res.push(value);
+      seen = computed;
+    } 
+    else if (iteratee) {
+      if (_.indexOf(seen, computed) === -1) {
+        seen.push(computed);
+        res.push(value);
+      } 
+    }
+    else if (_.indexOf(res, value) === -1) {
+      res.push(value);
+    }
+  });
 
-console.log(_.shuffle([1,2,3,4,5,6,7,8,9]));
+  return res;
+}
+
+_.map = function (list, iteratee, context) {
+  if (typeof iteratee !== 'function') return list;
+  iteratee = iteratee.bind(context);
+
+  const res = [];
+  _.each(list, function (value, i, list) {
+    res.push(iteratee(value, i, list));
+  });
+
+  return res;
+}
+
+_.pluck = function (obj, key) {
+  return _.map(obj, function (o) {
+    return o[key];
+  });
+}
+
+_.reduce = function (list, iteratee, memo, context) {
+  iteratee = iteratee.bind(context);
+  
+  _.each(list, function (value, i, list) {
+    if (memo === undefined) memo = value;
+    memo = iteratee(memo, value, i, list);
+  });
+  return memo;
+}
+
+_.contains = function (list, value, fromIndex = 0) {
+  if (Array.isArray(list)) {
+    for (let i = fromIndex; i < list.length; i++) {
+      if (list[i] === value) return true;
+    }
+  } else {
+    for (let key in list) {
+      if (list[key] === value) return true;
+    }
+  }
+  return false;
+}
+
+_.every = function (list, predicate, context) {
+  predicate = predicate.bind(context);
+
+  if (Array.isArray(list)) {
+    for (let i = 0; i < list.length; i++) {
+      if (!predicate(list[i], i, list)) return false;
+    }
+  } else {
+    for (let key in list) {
+      if (!predicate(list[key], key, list)) return false;
+    }
+  }
+  return true;
+}
+
+_.some = function (list, predicate, context) {
+  predicate = predicate.bind(context);
+
+  if (Array.isArray(list)) {
+    for (let i = 0; i < list.length; i++) {
+      if (predicate(list[i], i, list)) return true;
+    }
+  } else {
+    for (let key in list) {
+      if (predicate(list[key], key, list)) return true;
+    }
+  }
+  return false;
+}
+
+_.extend = function (destination, sources) {
+  if (typeof destination !== 'object' || Array.isArray(destination)) return destination;
+
+  for (i = 1; i < arguments.length; i++) {
+    const obj = arguments[i];
+    for (let key in obj) {
+      destination[key] = obj[key];
+    }
+  }
+
+  return destination;
+}
+
+_.defaults = function (object, defaults) {
+  if (typeof object !== 'object' || Array.isArray(object)) return object;
+
+  for (i = 1; i < arguments.length; i++) {
+    const obj = arguments[i];
+    for (let key in obj) {
+      if (object[key] === undefined) object[key] = obj[key];
+    }
+  }
+
+  return object;
+}
 
 module.exports = _;
--- spec/advanced-lowbar.spec.js
+++ spec/advanced-lowbar.spec.js
@@ -1,125 +1,347 @@
-/* global describe, it */
+/* global describe it */
 
-var _ = require('../advanced-lowbar');
-var expect = require('chai').expect;
-var sinon = require('sinon');
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const _ = require('../advanced-lowbar');
 
-describe('_', function () {
+describe ('_', () => {
+  it ('exists', () => {
+    expect(_).to.be.an('object');
+  });
 
-    describe('#indexOf', function () {
-        it('exists', function () {
-            expect(_.indexOf).to.be.a('function');
-        });
-        it('returns -1 if value is not in array', function () {
-            expect(_.indexOf([1,2,3], 4)).to.equal(-1);
-            expect(_.indexOf([], 0), 0).to.equal(-1);
-        });
-        it('returns the index of value', function () {
-            expect(_.indexOf([1,2,3], 2)).to.equal(1);
-            expect(_.indexOf([5,3,78,4,3,6,1,4], 78)).to.equal(2);
-            expect(_.indexOf([1,2,3,8,6,5], 5)).to.equal(5);
-            expect(_.indexOf([1], 1)).to.equal(0);
-        });
-        it('should work for strings', function () {
-            expect(_.indexOf('hello', 'e')).to.equal(1);
-            expect(_.indexOf(['a', 'b'], 'b')).to.equal(1);
-        });
-        it('should work for bools', function () {
-            expect(_.indexOf([true, false], false)).to.equal(1);
-        });
-        it('should use the binary search if iSorted is true', function () {
-            expect(_.indexOf([1,2,3,4,5,6,7,8,9], 7, true)).to.equal(6);
-            expect(_.indexOf([1,2,3,4,5,6,7,8,9], 7, false)).to.equal(6);
-        });
+  describe ('.identity', () => {
+    it ('returns the value used as the argument', () => {
+      expect(_.identity(5)).to.equal(5);
+    });
+    it ('returns the exact same object, not a copy', () => {
+      const obj = {};
+      expect(_.identity(obj)).to.equal(obj);
+    });
+  });
+
+  describe ('.first', () => {
+    it ('returns the first element in an array', () => {
+      expect(_.first([1,2,3,4,5])).to.equal(1);
+      expect(_.first([[1, 2, 3], 4, 5, 6])).to.eql([1, 2, 3]);
+      expect(_.first([{}, 4, 5])).to.eql({});
+    });
+    it ('returns the first n elements of an array', () => {
+      expect(_.first([1,2,3,4,5], 2)).to.eql([1, 2]);
+      expect(_.first([1, {}, [3, 4, 5], 4, 5], 3)).to.eql([1, {}, [3, 4, 5]]);
+    });
+    it ('returns whole array if n is bigger than the array length', () => {
+      expect(_.first([1, 2, 3], 100)).to.eql([1, 2, 3]);
+    });
+    it ('works for strings', () => {
+      expect(_.first('string')).to.equal('s');
+      expect(_.first('string', 3)).to.eql(['s', 't', 'r']);
+    });
+    it ('returns an empty array if n is less than 1', () => {
+      expect(_.first([1,2,3,4,5], -3)).to.eql([]);
+    });
+  });
+
+  describe ('.last', () => {
+    it ('returns the first element in an array', () => {
+      expect(_.last([3, 2, 1])).to.equal(1);
+      expect(_.last([[1, 2, 3]])).to.eql([1, 2, 3]);
+      expect(_.last([1, 2, 3, 4, 5, {}])).to.eql({});
+    });
+    it ('returns the last n elements of an array', () => {
+      expect(_.last([1,2,3,4,5], 2)).to.eql([4, 5]);
+      expect(_.last([1, {}, [3, 4, 5], 4, 5], 3)).to.eql([[3, 4, 5], 4, 5]);
+    });
+    it ('returns whole array if n is bigger than the array length', () => {
+      expect(_.last([1, 2, 3], 100)).to.eql([1, 2, 3]);
+    });
+    it ('works for strings', () => {
+      expect(_.last('string')).to.equal('g');
+      expect(_.last('string', 3)).to.eql(['i', 'n', 'g']);
+    });
+    it ('returns an empty array if n is less than 1', () => {
+      expect(_.last([1,2,3,4,5], -3)).to.eql([]);
+    });
+  });
+
+  describe ('.each', () => {
+    it ('returns the list for chaining', () => {
+      expect(_.each([1, 2, 3])).to.eql([1, 2, 3]);
+      expect(_.each({})).to.eql({});
+      expect(_.each('string')).to.eql('string');
+    });
+    it ('performs iteratee function for each element in an array', () => {
+      const spy = sinon.spy(function () {});
+      _.each([1, 2, 3], spy);
+      expect(spy.callCount).to.equal(3);
+      expect(spy.args).to.eql([
+        [1, 0, [1, 2, 3]], 
+        [2, 1, [1, 2, 3]], 
+        [3, 2, [1, 2, 3]]
+      ]);
+    });
+    it ('performs iteratee function for each char in a string', () => {
+      const spy = sinon.spy(function () {});
+      _.each('abc', spy);
+      expect(spy.callCount).to.equal(3);
+      expect(spy.args).to.eql([
+        ['a', 0, 'abc'], 
+        ['b', 1, 'abc'], 
+        ['c', 2, 'abc']
+      ]);
+    });
+    it ('performs iteratee function for each key-value pair in an object', () => {
+      const spy = sinon.spy(function () {});
+      _.each({0: 1, 1: 2, 2: 3}, spy);
+      expect(spy.callCount).to.equal(3);
+      expect(spy.args).to.eql([
+        [1, '0', {0: 1, 1: 2, 2: 3}], 
+        [2, '1', {0: 1, 1: 2, 2: 3}], 
+        [3, '2', {0: 1, 1: 2, 2: 3}]
+      ]);
+    });
+    it ('binds the iteratee function to the context', () => {
+      const fruits = {
+        names: ['apple', 'banana', 'orange'],
+        sayName: function (name) {
+          return name;
+        }
+      }
+      const res = [];
+      _.each(fruits.names, function (element) {
+        res.push(this.sayName(element));
+      }, fruits);
+
+      expect(res).to.eql(['apple', 'banana', 'orange']);
+
+      const context = {hello: 'context'};
+      const spy = sinon.spy(function () {});
+      _.each([1], spy, context);
+      expect(spy.thisValues).to.eql([{hello: 'context'}]); 
+    });
+  });
+
+  describe ('.indexOf', () => {
+    it ('return the first index where the value can be found', () => {
+      expect(_.indexOf([1, 2, 3], 2)).to.equal(1);
+      expect(_.indexOf([1, 2, 3], 1)).to.equal(0);
+      expect(_.indexOf([1, 2, 'a'], 'a')).to.equal(2);
+    });
+    it ('works for strings', () => {
+      expect(_.indexOf('abcdefg', 'b')).to.equal(1);
+    });
+    it ('returns -1 if value cannot be found', () => {
+      expect(_.indexOf([1, 2, 3], 4)).to.equal(-1);
+      expect(_.indexOf('123', '4')).to.equal(-1);
+    });
+    it ('uses a binary search if isSorted is true', () => {
+      expect(_.indexOf([1, 2, 3, 4, 5, 6, 7, 8], 7, true)).to.equal(6);
+    });
+  });
+
+  describe ('.filter', () => {
+    it ('returns the whole array if no predicate is passed', () => {
+      expect(_.filter([1, 2, 3])).to.eql([1, 2, 3]);
+    });
+    it ('returns an empty array if no element passes', () => {
+      expect(_.filter([1,2,3], n => n === 0)).to.eql([]);
+      expect(_.filter({}, n => n === 0)).to.eql([]);
+    });
+    it ('returns only the values that pass the predicate test', () => {
+      expect(_.filter([1,2,3,4,5], n => n%2 === 0)).to.eql([2,4]);
+    });
+    it ('binds the predicate to context', () => {
+      const context = {hello: 'context'};
+      const spy = sinon.spy(function () {});
+      _.filter([1], spy, context);
+      expect(spy.thisValues).to.eql([{hello: 'context'}]); 
+    });
+  });
+
+  describe ('.reject', () => {
+    it ('returns empty array if no predicate is passed or not an array', () => {
+      expect(_.reject([1, 2, 3])).to.eql([]);
+      expect(_.reject({}, n => n === 0)).to.eql([]);
+    });
+    it ('returns the whole array if no element passes', () => {
+      expect(_.reject([1,2,3], n => n === 0)).to.eql([1,2,3]);
+    });
+    it ('returns only the values that do not pass the predicate test', () => {
+      expect(_.reject([1,2,3,4,5], n => n%2 === 0)).to.eql([1,3,5]);
+    });
+    it ('binds the predicate to context', () => {
+      const context = {hello: 'context'};
+      const spy = sinon.spy(function () {});
+      _.reject([1], spy, context);
+      expect(spy.thisValues).to.eql([{hello: 'context'}]); 
+    });
+  });
+
+  describe ('.uniq', () => {
+    it ('returns the array wthout duplicates', () => {
+      expect(_.uniq([1, 1, 3, 2, 3, 4, 4, 3, 3])).to.eql([1, 3, 2, 4]);
+      expect(_.uniq([4, 3, 4, 4, 1])).to.eql([4, 3, 1]);
     });
 
-    describe('#once', function () {
-        it('exists', function () {
-            expect(_.once).to.be.a('function');
-        });
-        it('only allows a function to be called once', function () {
-            var spy = sinon.spy(function () {});
-            var tstFunc = _.once(spy);
-            expect(spy.callCount).to.equal(0);
-            tstFunc();
-            expect(spy.callCount).to.equal(1);
-            tstFunc();
-            expect(spy.callCount).to.equal(1);
-            tstFunc();
-            expect(spy.callCount).to.equal(1);
-        });
-        it('retains the functions err... functionality', function () {
-            var tstFunc = _.once(function (n) { return n * 2; });
-            expect(tstFunc(3)).to.equal(6);
-            tstFunc = _.once(function (a, b) { return a + b; });
-            expect(tstFunc(4, 7)).to.equal(11);
-        });
+    it ('works for strings', () => {
+      expect(_.uniq('abcb')).to.eql(['a', 'b', 'c']);
     });
 
-    describe('#memoize', function () {
-        it('exists', function () {
-            expect(_.memoize).to.be.a('function');
-        });
-        it('still performs the function\'s job', function () {
-            var fibonacci = _.memoize(function (n) {
-                return n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2);
-            });
-            expect(fibonacci(10)).to.equal(55);
-        });
-        it('uses a cache to store previous values', function () {
-            var spy = sinon.spy(function (n) {
-                return n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2);
-            });
-            var fibonacci = _.memoize(spy);
-            fibonacci(10);
-            expect(spy.callCount).to.equal(11);
-            fibonacci(10);
-            expect(spy.callCount).to.equal(11);
-            fibonacci(11);
-            expect(spy.callCount).to.equal(12);
-        });
+    it ('uses the iteratee to compare properties of objects', () => {
+      let people = [{ name: 'John', age: 20 }, { name: 'Mary', age: 31 }, { name: 'Kevin', age: 20 }];
+      let expected = [ { age: 20, name: "John" }, { age: 31, name: "Mary" } ];
+      expect(_.uniq(people, false, function (p) { return p.age; })).to.eql(expected);
+    });
+  });
+
+  describe ('.map', () => {
+    const context = {a: 'context'};
+    const spy = sinon.spy(function () {});
+    _.map([1, 2], spy, context);
+    it ('calls iteratee correct number of times', () => {
+      expect(spy.callCount).to.equal(2);
+    });
+    it ('calls the iteratee with the correct args', () => {
+      expect(spy.args).to.eql([
+        [1, 0, [1, 2]], 
+        [2, 1, [1, 2]]
+      ]);
+    });
+    it ('calls iteratee with the correct context', () => {
+      expect(spy.thisValues[0]).to.eql({a: 'context'});
+    });
+    it ('returns a new array', () => {
+      function doubleNUm (n) { return n * 2; };
+      expect(_.map([1,2,3], doubleNUm)).to.eql([2,4,6]);
+    });
+    it ('maps over objects', () => {
+      function doubleNUm (n) { return n * 2; };
+      expect(_.map({0:1,1:2,2:3}, doubleNUm)).to.eql([2,4,6]);
+    });
+  });
+
+  describe ('.pluck', () => {
+    it ('uses map to grab an array of object properties', () => {
+      const obj = [{'foo':100, 'bar':200}, {'foo':1, 'bar':2}, {'foo':50, 'bar':20}];
+      expect(_.pluck(obj, 'bar')).to.eql([200, 2, 20]);
+    });
+  });
+
+  describe ('.reduce', () => {
+    const context = {a: 'context'};
+    const spy = sinon.spy(function (acc, n) {return acc+n});
+    _.reduce([1, 2], spy, 0, context);
+    it ('calls iteratee correct number of times', () => {
+      expect(spy.callCount).to.equal(2);
+    });
+    it ('calls the iteratee with the correct args', () => {
+      expect(spy.args).to.eql([
+        [0, 1, 0, [1, 2]], 
+        [1, 2, 1, [1, 2]]
+      ]);
+    });
+    it ('calls iteratee with the correct context', () => {
+      expect(spy.thisValues[0]).to.eql({a: 'context'});
     });
 
-    describe('#delay', function () {
-        it('exists', function () {
-            expect(_.delay).to.be.a('function');
-        });
-        it('still performs the function passed to it', function () {
-            var res = 0;
-            var spy = sinon.spy(function (a) { res = a * 2; });
-            var clock = sinon.useFakeTimers();
-            _.delay(spy, 0, 2);
-            clock.tick(1);
-            expect(res).to.equal(4);
-        });
-        it('gets passes through any number of arguments to func', function () {
-            var spy = sinon.spy(function () {});
-            var clock = sinon.useFakeTimers();
-            _.delay(spy, 0, 'arg1', 5, 'yogurt', []);
-            clock.tick(1);
-            expect(spy.args[0]).to.eql(['arg1', 5, 'yogurt', []]);
-        });
-        it('delays the function call by so many ms', function () {
-            var spy = sinon.spy(function () {});
-            var clock = sinon.useFakeTimers();
-            _.delay(spy, 1000);
-            clock.tick(999);
-            expect(spy.callCount).to.equal(0);
-            clock.tick(2);
-            expect(spy.callCount).to.equal(1);
-        });
+    it ('can reduce to an array', () => {
+      expect(_.reduce([1,2,3,4,5], function (acc, num) {
+        if (num % 2 === 0) {
+          acc.push(num*2);
+        }
+        return acc;
+      }, [])).to.eql([4,8]);
     });
+  });
 
-    describe('#shuffle', function () {
-        it('exists', function () {
-            expect(_.shuffle).to.be.a('function');
-        });
+  describe ('.contains', () => {
+    it ('returns false if list does not contain value', () => {
+      expect(_.contains([1,2,3,4], 10)).to.be.false;
+      expect(_.contains({'a':1, 'b':2}, 10)).to.be.false;
     });
-
-    describe('#invoke', function () {
-        it('exists', function () {
-            expect(_.invoke).to.be.a('function');
-        });
+    it ('returns true if list does contain value', () => {
+      expect(_.contains([1,2,3,4], 3)).to.be.true;
+      expect(_.contains({'a':1, 'b':2}, 1)).to.be.true;
     });
+    it ('if list is an array use fromIndex to only search values after that index', () => {
+      expect(_.contains([1,2,3,4,5,6,7,8,9], 2, 5)).to.be.false;
+      expect(_.contains([1,2,3,4,5,6,7,8,9], 9, 5)).to.be.true;
+    });
+  });
 
+  describe ('.every', () => {
+    it ('returns false if one or more elements don\'t pass the predicate', () => {
+      function isEven (n) {return n%2 === 0};
+      function isA (c) {return c==='a'};
+      expect(_.every([1, 2, 3], isEven)).to.be.false;
+      expect(_.every('aaaba', isA)).to.be.false;
+      expect(_.every({'a':2, 'b':3}, isEven)).to.be.false;
+    });
+    it ('returns true if all elements pass the predicate', () => {
+      function isEven (n) {return n%2 === 0};
+      function isA (c) {return c==='a'};
+      expect(_.every([6, 2, 4], isEven)).to.be.true;
+      expect(_.every('aaaa', isA)).to.be.true;
+      expect(_.every({'a':2, 'b':8}, isEven)).to.be.true;
+    });
+    it ('binds the predicate to the context', () => {
+      const spy = sinon.spy(function () {});
+      const context = {'a': 'context'}
+      _.every([1, 2, 3], spy, context);
+      expect(spy.thisValues[0]).to.eql({'a': 'context'});
+    });
+  });
+
+  describe ('.some', () => {
+    it ('returns false if all elements don\'t pass the predicate', () => {
+      function isEven (n) {return n%2 === 0};
+      function isA (c) {return c==='a'};
+      expect(_.some([1, 5, 3], isEven)).to.be.false;
+      expect(_.some('bcd', isA)).to.be.false;
+      expect(_.some({'a':5, 'b':3}, isEven)).to.be.false;
+    });
+    it ('returns true if any elements pass the predicate', () => {
+      function isEven (n) {return n%2 === 0};
+      function isA (c) {return c==='a'};
+      expect(_.some([6, 1, 3], isEven)).to.be.true;
+      expect(_.some('bnkudgwadwu', isA)).to.be.true;
+      expect(_.some({'a':2, 'b':5}, isEven)).to.be.true;
+    });
+    it ('binds the predicate to the context', () => {
+      const spy = sinon.spy(function () {});
+      const context = {'a': 'context'}
+      _.some([1, 2, 3], spy, context);
+      expect(spy.thisValues[0]).to.eql({'a': 'context'});
+    });
+  });
+
+  describe ('.extend', () => {
+    it ('returns destination if it is not an object', () => {
+      expect(_.extend(5, {'a':1})).to.eql(5);
+      expect(_.extend([], {'a':1})).to.eql([]);
+    });
+    it ('returns the original object if no sources given', () => {
+      expect(_.extend({})).to.eql({});
+      expect(_.extend({'a':1})).to.eql({'a':1});
+    });
+    it ('copies properties over', () => {
+      expect(_.extend({'a':1}, {'b':2}, {'c':3})).to.eql({'a':1,'b':2,'c':3});
+    });
+    it ('overwrites previous entries for the same key', () => {
+      expect(_.extend({'a':1}, {'a':2}, {'a':100})).to.eql({'a':100});
+    });
+  });
+
+  describe ('.defaults', () => {
+    it ('returns destination if it is not an object', () => {
+      expect(_.defaults(5, {'a':1})).to.eql(5);
+      expect(_.defaults([], {'a':1})).to.eql([]);
+    });
+    it ('returns the original object if no sources given', () => {
+      expect(_.defaults({})).to.eql({});
+      expect(_.defaults({'a':1})).to.eql({'a':1});
+    });
+    it ('only copies properties over if the destination does not already have that key', () => {
+      expect(_.defaults({'a':1}, {'b':2}, {'c':3}, {'a':100})).to.eql({'a':1,'b':2,'c':3});
+    });
+  });
 });
